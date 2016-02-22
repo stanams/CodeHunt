35b00b3c7fea301c672ba0712566d2d5b639d201
Your Actual Name <stan.amsellem@gmail.com>
2016-02-22 19:55:22 +0100

    fix small mistake

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./codehunt.jsx",
+  entry: "./frontend/codehunt.jsx",
   output: {
     path: "./",
     filename: "bundle.js"
