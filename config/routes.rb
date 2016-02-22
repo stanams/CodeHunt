e861ec0f6b4d44f9846b685d9c4ed54900f5bc7f
Your Actual Name <stan.amsellem@gmail.com>
2016-02-22 21:06:55 +0100

    make product controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "static_pages#root"
 
+namespace :api, default: {format: :json} do
   resources :users, only: [:index, :new, :create]
   resource :session, only: [:create, :destroy]
+  resources :products
+end
 
 end
