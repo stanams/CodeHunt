9080927eb3b464a626b4ca4b9b8766e713e995b0
Your Actual Name <stan.amsellem@gmail.com>
2016-02-23 09:55:22 +0100

    update bundle & bundle map path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   context: __dirname,
   entry: "./frontend/codehunt.jsx",
   output: {
-    path: "./",
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
   module: {
