2e82c31c57d083fcd38b59c95b09aa975923ea4f
Your Actual Name <stan.amsellem@gmail.com>
2016-02-24 09:33:53 +0100

    create product actions

---
+++ frontend/actions/product_actions.js
@@ -0,0 +1,41 @@
+var Dipsatcher = require('../dispatcher/dispatcher');
+var ProductConstants = require('../constants/product_constants');
+
+module.exports = {
+
+  receiveAllProducts: function(){
+    Dispatcher.dispatch({
+      actionType: ProductConstants.PRODUCTS_RECEIVED,
+      products: products
+    });
+  },
+
+  receiveSingleProduct: function(){
+    Dispatcher.dispatch({
+      actionType: ProductConstants.PRODUCT_RECEIVED,
+      product: product
+    });
+  },
+
+  createProduct: function(){
+    Dispatcher.dispatch({
+      actionType: ProductConstants.PRODUCT_CREATED,
+      product: product
+    });
+  },
+
+  updateProduct: function(){
+    Dispatcher.dispatch({
+      actionType: ProductConstants.PRODUCT_UPDATED,
+      product: product
+    });
+  },
+
+  destroyProduct: function(){
+    Dispatcher.dispatch({
+      actionType: ProductConstants.PRODUCT_DESTROYED,
+      product: ''
+    });
+  },
+
+}
